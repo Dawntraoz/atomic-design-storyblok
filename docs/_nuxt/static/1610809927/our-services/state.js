a6414247a489cf74637a5eb32d88553918aaf824
Dawntraoz <albasilventefu@gmail.com>
2021-01-21 22:08:49 +0100

    feat: update docs

--- docs/_nuxt/static/1610809927/our-services/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002Fatomic-design-storyblok\u002F_nuxt\u002Fstatic\u002F1610809927",layout:"default",error:null,state:{i18n:{routeParams:{en:{slug:"our-services"},es:{slug:"nuestros-servicios"}}}},serverRendered:true,routePath:"\u002Four-services",config:{}};
