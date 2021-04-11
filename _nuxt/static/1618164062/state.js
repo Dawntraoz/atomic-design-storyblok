b181e07cf3f1620322e717b92b42b58afbc29136
Dawntraoz <Dawntraoz@users.noreply.github.com>
2021-04-11 18:07:06 +0000

    deploy: 8e8c7ec0e8a56af9ffe102caf0125b08616e638d

--- _nuxt/static/1618164062/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002Fatomic-design-storyblok\u002F_nuxt\u002Fstatic\u002F1618164062",layout:"default",error:{statusCode:401,message:{error:"Unauthorized"}},state:{i18n:{routeParams:{}}},serverRendered:false,routePath:"\u002F",config:{_app:{basePath:"\u002Fatomic-design-storyblok\u002F",assetsPath:"\u002Fatomic-design-storyblok\u002F_nuxt\u002F",cdnURL:null}}};
