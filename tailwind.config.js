cef2573547272ccece872f4f8a8f4bd87b145ef8
Dawntraoz <albasilventefu@gmail.com>
2022-02-18 15:27:31 +0100

    feat: upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,13 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
-  purge: [
+  content: [
     './components/**/*.{vue,js}',
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
